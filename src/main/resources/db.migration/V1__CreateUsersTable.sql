b8006e701847eb1581dfcbed34bd7c77a5f25825
neelima.katari@thoughtworks.com <neelima.katarithoughtworks.com@Admins-MBP.lan>
2022-05-10 21:15:34 +0530

    Initial Commit

---
+++ src/main/resources/db.migration/V1__CreateUsersTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE users (
+    ID int NOT NULL PRIMARY KEY,
+    Name varchar(40) NOT NULL,
+    Address varchar(200)
+);
---
+++ src/main/resources/db.migration/V2__CreateProductsTable.sql
@@ -0,0 +1,5 @@
+CREATE TABLE products (
+    ID int NOT NULL PRIMARY KEY,
+    Name varchar(40) NOT NULL,
+    PRICE DOUBLE DEFAULT 0.0
+);
